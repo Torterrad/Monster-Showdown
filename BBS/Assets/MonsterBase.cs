3b418482c5ab4d3104ec1c96a63a84104ad86e7a
Torterrad <64802272+Torterrad@users.noreply.github.com>
2023-12-03 13:54:12 +0000

    First commit

---
+++ BBS/Assets/MonsterBase.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Monster", menuName = "Monster/Create new monster")]
+public class MonsterBase : ScriptableObject
+{
+   [SerializeField] string name;
+
+    [TextArea]
+    [SerializeField] string description;
+
+    [SerializeField] Sprite monSprite;
+
+    [SerializeField] MonsterType type1;
+    [SerializeField] MonsterType type2;
+
+    [SerializeField] int maxHp;
+    [SerializeField] int physical_Attack;
+    [SerializeField] int physical_Defense;
+    [SerializeField] int special_Attack;
+    [SerializeField] int special_Defense;
+    [SerializeField] int speed;
+
+    public string Name
+    {
+        get { return name; }
+    }
+
+    public string Description
+    {
+        get { return description; }
+    }
+
+    public Sprite MonSprite
+    {
+        get { return monSprite; }
+    }
+    public MonsterType Type1
+    {
+        get { return type1; }
+    }
+    public MonsterType Type2
+    {
+        get { return type1; }
+    }
+    public int MaxHp
+    {
+        get { return maxHp; }
+    }
+    public int PhysicalAttack
+    {
+        get { return physical_Attack; }
+    }
+    public int PhysicalDefense
+    {
+        get { return physical_Defense; }
+    }
+    public int SpecialAttack
+    {
+        get { return special_Attack; }
+    }  
+    public int SpecialDefense
+    {
+        get { return special_Defense; }
+    }
+    public int Speed
+    {
+        get { return speed; }
+    }
+
+
+
+
+}
+
+public enum MonsterType
+{
+    None,
+    Fire,
+    Water,
+    Grass,
+    Earth,
+    Air,
+    Electric,
+    Metal,
+    Ice,
+    
+
+}
